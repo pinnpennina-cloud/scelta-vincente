db2e1d3fc7d91734da854b75954a57cbc3466cfc
pinnpennina-cloud <pinnpennina@gmail.com>
2026-08-16 22:14:14 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // Sostituisci con il tuo dominio reale
-  site: 'https://tuodominio.it',
+  site: '/scelta-vincente',
 });
