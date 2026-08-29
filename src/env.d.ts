dfef0ea6c5d059add5114e1d54c6162525f82272
pinnpennina-cloud <pinnpennina@gmail.com>
2026-08-29 08:47:53 +0200

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly META_PAGE_ID: string;
+  readonly META_PAGE_ACCESS_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
